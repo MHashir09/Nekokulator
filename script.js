29aa56579618aa923b4e2616ed6f764300536157
MHashir09 <hashirzubair5742@gmail.com>
2025-10-01 14:40:58 +0500

    feat: Make percentage operator work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const calcBody = document.querySelector("#cal-body");
+const calcPage = document.querySelector("body");
 const calcScreen = document.querySelector(".screen");
 
 let currentNumber = "";
@@ -7,7 +7,8 @@ let accumulator = null;
 let unaryFlag = false;
 let resultFlag = false;
 
-calcBody.addEventListener("click", handleEventListeners);
+calcPage.addEventListener("click", handleButtonEventListeners);
+calcPage.addEventListener("keydown", handleKeyboardEventListeners)
 
 function findSum(num1, num2) {
   return +num1 + +num2;
@@ -25,13 +26,23 @@ function findRatio(num1, num2) {
   return +num1 / +num2;
 }
 
-function handleNumbers(button) {
-  const newNumber = button.textContent;
+function findPercentage(num1, num2) {
+  return (+num1 * +num2) / 100;
+}
 
-  if (resultFlag == true) {
-    calcScreen.textContent = ''; // to clear the screen if user clicks any number
-    resultFlag = false;
+function handleDelete() {
+  if (currentNumber != "|") {
+    currentNumber = currentNumber.slice(0, -1);
+    calcScreen.textContent = calcScreen.textContent.slice(0, -1);
   }
+
+  if (calcScreen.textContent === "") {
+     calcScreen.textContent = "|";
+  }
+}
+
+function handleNumbers(button) {
+  const newNumber = button;
 
   if (newNumber == '.' && currentNumber.includes('.')) return;
 
@@ -40,13 +51,13 @@ function handleNumbers(button) {
 }
 
 function handleOperators(button) {
-  const newOperator = button.textContent;
+  const newOperator = button;
 
-  if (currentNumber == "" && unaryFlag == false) {
+  if (currentNumber == "" && unaryFlag == false && resultFlag != true) {
     if (newOperator == '-' || newOperator == '+') {
-    currentNumber += newOperator;
-    calcScreen.textContent += `(${newOperator}`;
-    unaryFlag = true;
+      currentNumber += newOperator;
+      calcScreen.textContent += `(${newOperator}`;
+      unaryFlag = true;
     }
   } else if (pendingOperator == "") {
     if (unaryFlag == true) calcScreen.textContent += ')'; // to show a closing parenthesis if unary was used
@@ -66,11 +77,13 @@ function handleOperators(button) {
 }
 
 function handleSpecialButtons(button) {
-  const buttonContent = button.textContent;
+  const buttonContent = button;
 
-  if (buttonContent == "=") {
+  if (buttonContent == "=" || buttonContent == "Enter") {
     accumulator = calculate(accumulator, pendingOperator, currentNumber);
-    (accumulator == 'Infinity') ? calcScreen.textContent = 'MATH ERROR' : calcScreen.textContent = accumulator;
+
+    calcScreen.textContent = (accumulator == 'Infinity') ? 'MATH ERROR' : accumulator;
+
     currentNumber = "";
     pendingOperator = "";
     accumulator = null;
@@ -83,41 +96,79 @@ function handleSpecialButtons(button) {
     accumulator = null;
     unaryFlag = false;
     decimalFlag = false;
-    calcScreen.textContent = "";
+    calcScreen.textContent = "|";
+  } else if (buttonContent == "DEL" || buttonContent == "Backspace") {
+    if (calcScreen.textContent == '|') return;
+    handleDelete();
   }
 }
 
-function handleEventListeners(event) {
+function handleButtonEventListeners(event) {
   const button = event.target;
+
   if (!button.matches("button")) return;
 
-  if (button.classList.contains("numbers")) {
-    handleNumbers(button);
-  } else if (button.classList.contains("operators")) {
-    handleOperators(button);
-  } else {
-    handleSpecialButtons(button);
+  const input = button.textContent;
+  if (calcScreen.textContent == '|') calcScreen.textContent = '';
+
+  if (resultFlag == true) {
+    calcScreen.textContent = '|'; // to clear the screen if user clicks any number
+    resultFlag = false;
   }
+
+  if (button.classList.contains("numbers")) {
+    handleNumbers(input);
+  } else if (button.classList.contains("operators")) {
+    handleOperators(input);
+  } else {
+    handleSpecialButtons(input);
+  }
+}
+
+function handleKeyboardEventListeners(event) {
+  const allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', 'x', '/', '%', 'Enter', 'Backspace'];
+  const numberKeys = allowedKeys.slice(0, 10);
+  const operatorKeys = allowedKeys.slice(10, 15);
+  const specialKeys = allowedKeys.slice(15, 17);
+  const key = event.key;
+
+  if (!allowedKeys.includes(key)) return;
+  if (calcScreen.textContent == '|') calcScreen.textContent = '';
+
+  if (resultFlag == true) {
+    calcScreen.textContent = '|'; // to clear the screen if user clicks any number
+    resultFlag = false;
+  }
+
+  if (numberKeys.includes(key)) {
+    handleNumbers(key);
+  } else if (operatorKeys.includes(key)) {
+    handleOperators(key);
+  } else if (specialKeys.includes(key)) {
+    handleSpecialButtons(key);
+  }
+
 }
 
 function calculate(accumulator, pendingOperator, currentNumber) {
-  if (pendingOperator != "" && currentNumber != "") {
-    switch (pendingOperator) {
-      case "+":
-        accumulator = findSum(accumulator, currentNumber);
-        break;
-      case "-":
-        accumulator = findDifference(accumulator, currentNumber);
-        break;
-      case "x":
-        accumulator = findProduct(accumulator, currentNumber);
-        break;
-      case "/":
-        accumulator = findRatio(accumulator, currentNumber);
-        break;
-    }
-  } else {
-    calcScreen.textContent = "SYNTAX ERROR";
+  switch (pendingOperator) {
+    case "+":
+      accumulator = findSum(accumulator, currentNumber);
+      break;
+    case "-":
+      accumulator = findDifference(accumulator, currentNumber);
+      break;
+    case "x":
+      accumulator = findProduct(accumulator, currentNumber);
+      break;
+    case "/":
+      accumulator = findRatio(accumulator, currentNumber);
+      break;
+    case "%":
+      accumulator = findPercentage(accumulator, currentNumber);
+      break;
+    default:
+      calcScreen.textContent = "SYNTAX ERROR";
   }
   return accumulator;
 }
